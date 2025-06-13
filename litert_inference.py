b6e3a2a47614c76fbe8f764d3aa3b85622c05aae
Jeremy Lau <30300826+fdxmw@users.noreply.github.com>
2025-06-12 17:33:44 -0700

    Refactor litert_inference.py and add a test.
    The test just checks that inference with the LiteRT Interpreter is at least 75%
    accurate over the first 10 images in the test data after one training epoch.

--- litert_inference.py
+++ litert_inference.py
@@ -16,9 +16,20 @@ from ai_edge_litert.interpreter import Interpreter
 import inference_util
 import mnist_util
 import numpy as np
+import tensorflow as tf
 
 
-def normalize_input(interpreter, x):
+def load_tflite_model(model_file_name: str) -> Interpreter:
+    """Load the quantized model and return an initialized LiteRT Interpreter."""
+    # Set preserve_all_tensors so we can inspect intermediate tensor values.
+    # Intermediate values help when debugging other quantized inference implementations.
+    interpreter = Interpreter(
+        model_path=model_file_name, experimental_preserve_all_tensors=True)
+    interpreter.allocate_tensors()
+    return interpreter
+
+
+def normalize_input(interpreter: Interpreter, input: tf.Tensor):
     """Normalize input data to int8."""
     input_details = interpreter.get_input_details()[0]
 
@@ -36,100 +47,70 @@ def normalize_input(interpreter, x):
     #
     # Adding input_zero_point (-128) effectively converts the uint8 image data to int8,
     # by shifting the range [0, 255] to [-128, 127].
-    return x / input_scale + input_zero_point
+    return input / input_scale + input_zero_point
 
 
-# Helper function to run inference on a TFLite model
-def run_tflite_model(interpreter, start_image: int, num_images: int):
+def run_tflite_model(
+        interpreter: Interpreter, test_image: tf.Tensor) -> (tf.Tensor, tf.Tensor, int):
+    """Run inference on a single image with a TFLite model.
+
+    Returns (layer0_output, layer1_output, predicted_digit), where:
+
+    * `layer0_output` is the first layer's raw Tensor output (shape (1, 18)).
+    * `layer1_output` is the second layer's raw Tensor output (shape (1, 10)).
+    * `predicted_digit` is the actual predicted digit. It is equivalent to
+      `layer1_output.flatten().argmax()`.
+
+    """
     input_details = interpreter.get_input_details()[0]
     output_details = interpreter.get_output_details()[0]
 
-    correct = 0
-    for test_index in range(start_image, start_image + num_images):
-        interpreter.reset_all_variables()
+    interpreter.reset_all_variables()
 
-        test_image = test_images[test_index]
+    # If the input type is quantized, rescale input data.
+    if input_details["dtype"] == np.int8:
+        test_image = normalize_input(interpreter, test_image)
 
-        print(f"network input (#{test_index}):")
-        inference_util.display_image(test_image)
+    # Add the batch dimension and convert from float32 to the actual input type.
+    test_image = np.expand_dims(test_image, axis=0).astype(input_details["dtype"])
+    interpreter.set_tensor(input_details["index"], test_image)
+    interpreter.invoke()
 
-        # If the input type is quantized, rescale input data.
-        if input_details["dtype"] == np.int8:
-            test_image = normalize_input(interpreter, test_image)
+    # Tensor metadata, from the Model Explorer
+    # (https://github.com/google-ai-edge/model-explorer):
+    #
+    # tensor 0: input          int8[1, 12, 12]
+    #
+    # tensor 1: reshape shape  int32[2]
+    # tensor 2: reshape output int8[1, 144]
+    #
+    # tensor 3: layer 0 weight int8[18, 144]
+    # tensor 4: layer 0 bias   int32[18]
+    # tensor 5: layer 0 output int8[1, 18]
+    #
+    # tensor 6: layer 1 weight int8[10, 18]
+    # tensor 7: layer 1 bias   int32[10]
+    # tensor 8: layer 1 output int8[1, 10]
 
-        # Add the batch dimension and convert from float32 to the actual input type.
-        test_image = np.expand_dims(test_image, axis=0).astype(input_details["dtype"])
-        print("test_image", test_image.shape, test_image.dtype, "\n")
-        interpreter.set_tensor(input_details["index"], test_image)
-        interpreter.invoke()
+    # Retrieve and display the first layer's output. This is not necessary for
+    # correctness, but useful for debugging differences between inference
+    # implementations.
+    layer0_output_index = 5
+    layer0_output = interpreter.get_tensor(layer0_output_index)
 
-        # Tensor metadata, from the Model Explorer
-        # (https://github.com/google-ai-edge/model-explorer):
-        #
-        # tensor 0: input          int8[1, 12, 12]
-        #
-        # tensor 1: reshape shape  int32[2]
-        # tensor 2: reshape output int8[1, 144]
-        #
-        # tensor 3: layer 0 weight int8[18, 144]
-        # tensor 4: layer 0 bias   int32[18]
-        # tensor 5: layer 0 output int8[1, 32]
-        #
-        # tensor 6: layer 1 weight int8[10, 18]
-        # tensor 7: layer 1 bias   int32[10]
-        # tensor 8: layer 1 output int8[1, 10]
+    # Retrieve and display the second layer's output.
+    layer1_output_index = 8
+    layer1_output = interpreter.get_tensor(layer1_output_index)
 
-        # Retrieve and display the first layer's output. This is not necessary for
-        # correctness, but useful for debugging differences between inference
-        # implementations.
-        layer0_output_index = 5
-        layer0_output = interpreter.get_tensor(layer0_output_index)
-        print(
-            f"layer 0 output (tensor index {layer0_output_index})",
-            layer0_output.shape,
-            layer0_output.dtype,
-            "\n",
-            layer0_output,
-            "\n",
-        )
+    output = interpreter.get_tensor(output_details["index"])[0]
+    output_scale, output_zero_point = output_details["quantization"]
 
-        # Retrieve and display the second layer's output.
-        layer1_output_index = 8
-        layer1_output = interpreter.get_tensor(layer1_output_index)
-        print(
-            f"layer 1 output (tensor index {layer1_output_index})",
-            layer1_output.shape,
-            layer1_output.dtype,
-            "\n",
-            layer1_output,
-            "\n",
-        )
+    # Second layer's flattened output should be the model's output.
+    assert np.logical_and.reduce(layer1_output.flatten() == output)
 
-        output = interpreter.get_tensor(output_details["index"])[0]
-        output_scale, output_zero_point = output_details["quantization"]
+    return layer0_output, layer1_output, output.argmax()
 
-        # Second layer's output should be the model's output.
-        assert np.logical_and.reduce(layer1_output == output, axis=None)
-
-        actual = output.argmax()
-        expected = test_labels[test_index]
-        print(f"network output (#{test_index}):")
-        inference_util.display_outputs(output, expected, actual)
-
-        if actual == expected:
-            correct += 1
-
-        if test_index < num_images - 1:
-            print()
-
-    if num_images > 1:
-        print(
-            f"{correct}/{num_images} correct predictions, "
-            f"{100.0 * correct / num_images:.0f}% accuracy"
-        )
-
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(prog="litert_inference.py")
     parser.add_argument("--start_image", type=int, default=0,
                         help="Starting image index in the MNIST test dataset")
@@ -143,14 +124,37 @@ if __name__ == "__main__":
     # Load MNIST dataset.
     _, (test_images, test_labels) = mnist_util.load_mnist_images()
 
-    # Load the quantized model and initialize the LiteRT interpreter. Set
-    # preserve_all_tensors so we can inspect intermediate tensor values. Intermediate
-    # values help when debugging alternative quantized inference implementations like
-    # numpy_inference.py
-    tflite_file = "quantized.tflite"
-    interpreter = Interpreter(
-        model_path=tflite_file, experimental_preserve_all_tensors=True)
-    interpreter.allocate_tensors()
+    interpreter = load_tflite_model(model_file_name="quantized.tflite")
 
-    run_tflite_model(
-        interpreter, start_image=args.start_image, num_images=args.num_images)
+    correct = 0
+    for test_index in range(args.start_image, args.start_image + args.num_images):
+        test_image = test_images[test_index]
+        print(f"network input (#{test_index}):")
+        inference_util.display_image(test_image)
+        print("test_image", test_image.shape, test_image.dtype, "\n")
+
+        layer0_output, layer1_output, actual = run_tflite_model(
+            interpreter=interpreter, test_image=test_image)
+
+        print(f"layer 0 output {layer0_output.shape} {layer0_output.dtype}")
+        print(f"{layer0_output}\n")
+        print(f"layer 1 output {layer1_output.shape} {layer1_output.dtype}")
+        print(f"{layer1_output}\n")
+
+        expected = test_labels[test_index]
+        print(f"network output (#{test_index}):")
+        inference_util.display_outputs(layer1_output.flatten(), expected, actual)
+        if actual == expected:
+            correct += 1
+
+        if test_index < args.num_images - 1:
+            print()
+
+    if args.num_images > 1:
+        print(
+            f"{correct}/{args.num_images} correct predictions, "
+            f"{100.0 * correct / args.num_images:.0f}% accuracy"
+        )
+
+if __name__ == "__main__":
+    main()
---
+++ litert_inference_test.py
@@ -0,0 +1,41 @@
+import unittest
+
+import litert_inference
+import mnist_util
+import tensorflow_training
+
+class TestLiteRTInference(unittest.TestCase):
+    def setUp(self):
+        (train_images, train_labels), (self.test_images, self.test_labels) = (
+            mnist_util.load_mnist_images())
+        model_file_name="quantized.tflite"
+        learning_rate = 0.001
+        epochs = 1
+
+        model = tensorflow_training.train_unquantized_model(
+            learning_rate=learning_rate, epochs=epochs,
+            train_images=train_images, train_labels=train_labels)
+        model = tensorflow_training.quantize_model(
+            model=model, learning_rate=learning_rate / 10000, epochs=epochs,
+            train_images=train_images, train_labels=train_labels,
+            model_file_name=model_file_name)
+        self.interpreter = litert_inference.load_tflite_model(
+            model_file_name=model_file_name)
+
+    def test_litert_inference(self):
+        num_images = 10
+        correct = 0
+        for test_index in range(num_images):
+            _, _, actual = litert_inference.run_tflite_model(
+                interpreter=self.interpreter, test_image=self.test_images[test_index])
+            expected = self.test_labels[test_index]
+
+            if actual == expected:
+                correct += 1
+
+        accuracy = correct / num_images
+        self.assertTrue(accuracy > 0.75)
+
+
+if __name__ == "__main__":
+    unittest.main()
